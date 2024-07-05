8a7d9cc0722ac6cc4371ba24aac7f3aea9f71bdb
Mariela <mariela.hernandezchacon@ucr.ac.cr>
2024-07-04 18:18:43 -0600

    2.c

---
+++ impl_func/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Función para determinar si un número es par (1) o impar (0)
+int esPar(int num) {
+    // Completa la función aquí
+}
+
+int main() {
+    int numero = 17;
+    if (esPar(numero))
+        printf("%d es un número par.\n", numero);
+    else
+        printf("%d es un número impar.\n", numero);
+    return 0;
+}
+
