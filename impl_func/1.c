9285241fd5b574a13ce090ad696f24b876a21c95
Mariela <mariela.hernandezchacon@ucr.ac.cr>
2024-07-04 18:15:56 -0600

    impl 1.c

---
+++ impl_func/1.c
@@ -0,0 +1,15 @@
+// Función incompleta
+#include <stdio.h>
+
+// Función para calcular el factorial de un número
+int factorial(int n) {
+    // Completa la función aquí
+}
+
+int main() {
+    int num = 5;
+    int resultado = factorial(num);
+    printf("El factorial de %d es: %d\n", num, resultado);
+    return 0;
+}
+
