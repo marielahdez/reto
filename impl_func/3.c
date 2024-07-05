0fb6d4b886edf85f969793ad90d86a5a3c1b8144
Mariela <mariela.hernandezchacon@ucr.ac.cr>
2024-07-04 18:22:14 -0600

    3.c

---
+++ impl_func/3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Función para convertir grados Celsius a Fahrenheit
+float celsiusToFahrenheit(float celsius) {
+    // Completa la función aquí
+}
+
+int main() {
+    float temperaturaCelsius = 25.5;
+    float temperaturaFahrenheit = celsiusToFahrenheit(temperaturaCelsius);
+    printf("%.2f grados Celsius son %.2f grados Fahrenheit.\n", temperaturaCelsius, temperaturaFahrenheit);
+    return 0;
+}
+
