aa5e8a8c873848869af646111b36cd2a373709a9
Mariela <mariela.hernandezchacon@ucr.ac.cr>
2024-07-04 18:27:41 -0600

    cambiar 4.c

--- impl_func/4.c
+++ impl_func/4.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-// Función para calcular el factorial de un número
-int factorial(int n) {
-    // Completa la función aquí
+// Función para verificar si un año es bisiesto
+int esBisiesto(int year) {
+    // Implementar función aquí
 }
 
 int main() {
-    int num = 5;
-    int resultado = factorial(num);
-    printf("El factorial de %d es: %d\n", num, resultado);
+    int año = 2024;
+    if (esBisiesto(año))
+        printf("%d es un año bisiesto.\n", año);
+    else
+        printf("%d no es un año bisiesto.\n", año);
     return 0;
 }
-
